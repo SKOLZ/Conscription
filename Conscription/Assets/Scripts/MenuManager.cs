e8a31d69c2caad263855709406f94cc09724a432
SKOLZ <gabriel.zanzotti@gmail.com>
2015-08-01 21:44:27 -0300

    menu ready.

---
+++ Conscription/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	public GameObject instructionsPanel;
+	public GameObject playButton;
+	public GameObject instructionsButton;
+	public GameObject quitButton;
+
+	public void startGame() {
+		Application.LoadLevel("Main");
+	}
+
+	public void openInstructions() {
+		instructionsPanel.SetActive (true);
+		playButton.SetActive (false);
+		instructionsButton.SetActive (false);
+		quitButton.SetActive (false);
+	}
+
+	public void closeInstructions() {
+		instructionsPanel.SetActive (false);
+		playButton.SetActive (true);
+		instructionsButton.SetActive (true);
+		quitButton.SetActive (true);
+	}
+
+	public void quit() {
+		Application.Quit ();
+	}
+
+}
